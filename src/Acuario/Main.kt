05c3a9774315f4161192e2b19aedc5ce6abf0c10
ruberthtapara <ruberth.tapara@tecsup.edu.pe>
2026-03-29 01:06:14 -0500

    agregacion de interfaces para PecesAcuario

--- src/Acuario/Main.kt
+++ src/Acuario/Main.kt
@@ -5,35 +5,31 @@ fun crearPeces() {
     val miTiburon = Tiburon()
     val miPezPayaso = PezPayaso()
     println("El tiburón es de color: ${miTiburon.color}")
+    print("El tiburón suele: ")
+    miTiburon.comer()
     println("El pez payaso es de color: ${miPezPayaso.color}")
+    print("El pez payaso suele: ")
+    miPezPayaso.comer()
 }
 fun construirAcuario() {
     val acuario2 = Acuario(ancho = 25)
     acuario2.imprimirTamano()
-
     val acuario3 = Acuario(alto = 35, largo = 110)
     acuario3.imprimirTamano()
-
     val acuario4 = Acuario(ancho = 25, alto = 35, largo = 110)
     acuario4.imprimirTamano()
-
     val acuario6 = Acuario(numeroDePeces = 29)
     acuario6.imprimirTamano()
-
     val acuario7 = Acuario(numeroDePeces = 29)
     acuario7.imprimirTamano()
     acuario7.volumen = 70
     acuario7.imprimirTamano()
-
     val acuario8 = Acuario(largo = 25, ancho = 25, alto = 40)
     acuario8.imprimirTamano()
-
     val miAcuario = Acuario(ancho = 25, largo = 25, alto = 40)
     miAcuario.imprimirTamano()
-
     val miTorre = TanqueTorre(diametro = 25, alto = 40)
     miTorre.imprimirTamano()
-
     println(" Color de los Peces ")
     crearPeces()
 }
--- src/Acuario/PecesAcuario.kt
+++ src/Acuario/PecesAcuario.kt
@@ -1,10 +1,20 @@
 package acuario
+
+interface AccionPez {
+    fun comer()
+}
 abstract class Pez {
     abstract val color: String
 }
-class Tiburon : Pez() {
+class Tiburon : Pez(), AccionPez {
     override val color: String = "gris"
+    override fun comer() {
+        println("cazar y comer peces peqweños como grandes")
+    }
 }
-class PezPayaso : Pez() {
+class PezPayaso : Pez(), AccionPez {
     override val color: String = "dorado"
+    override fun comer() {
+        println("comer algas dulces")
+    }
 }
